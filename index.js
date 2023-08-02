94573a7314da273f5909bf54a25768100e5176e0
faiz-webdev <faijuddin@tothenew.com>
2023-08-02 17:10:17 +0530

    3. action dispatch

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const store = createStore(reducer);
 
 // reducer
 function reducer(state = { amount: 1 }, action) {
+    if(action.type==='increment')
+        return { amount: state.amount+1}
   return state;
 }
 
 //global state
+console.log(store.getState());
+
+//action
+store.dispatch({type:'increment'})
 console.log(store.getState());
