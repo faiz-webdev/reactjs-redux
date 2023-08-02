89a33e61759dfc8ce0aab8cb7ce06716918b34e2
faiz-webdev <faijuddin@tothenew.com>
2023-08-02 17:21:33 +0530

    5. main state change

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ import { createStore } from "redux";
 //store
 const store = createStore(reducer);
 
+const history = [];
+
 // reducer
 function reducer(state = { amount: 1 }, action) {
-    if(action.type==='increment')
-    // state.amount= state.amount+1
-        return { amount: state.amount+1}
+  //state immutibility
+  if (action.type === "increment")
+    state.amount= state.amount+1
+    // return { amount: state.amount + 1 };
   return state;
 }
 
 //global state
-store.subscribe(()=> {
-    console.log(store.getState())
-})
+store.subscribe(() => {
+  //   console.log(store.getState());
+  history.push(store.getState());
+  console.log(history);
+});
 
 //action
-store.dispatch({type:'increment'})
+// store.dispatch({type:'increment'})
+setInterval(() => {
+  store.dispatch({ type: "increment" });
+}, 500);
