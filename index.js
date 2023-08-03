b9abc915ac0ced1d322eb11123c469384f296449
faiz-webdev <faijuddin@tothenew.com>
2023-08-03 09:50:09 +0530

    13. redux thunk with async

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 //action name constants
 const inc = "increment";
@@ -8,7 +10,10 @@ const incByAmt = "incrementByAmount";
 const init = "init";
 
 //store
-const store = createStore(reducer, applyMiddleware(logger.default));
+const store = createStore(
+  reducer,
+  applyMiddleware(logger.default, thunk.default)
+);
 
 const history = [];
 
@@ -35,6 +40,13 @@ function reducer(state = { amount: 1 }, action) {
 //   console.log(history);
 // });
 
+//Async API call
+async function getUser() {
+  const { data } = await axios.get("http://localhost:3000/accounts/1");
+  console.log(data);
+}
+// getUser();
+
 //action creator
 function increment() {
   return { type: inc };
@@ -48,13 +60,16 @@ function incrementByAmount(value) {
   return { type: incByAmt, payload: value };
 }
 
-function initUser(value) {
-  return { type: init, payload: value };
+async function initUser(dispatch, getState) {
+  //   return { type: init, payload: value };
+  const { data } = await axios.get("http://localhost:3000/accounts/1");
+  //   return { type: init, payload: data.amount };
+  dispatch({ type: init, payload: data.amount });
 }
 
 setInterval(() => {
   //   store.dispatch(incrementByAmount(5));
   //   store.dispatch({ type: "decrement" });
   //   store.dispatch({ type: "incrementByAmount", payload: 4 });
-  store.dispatch(initUser(22));
+  store.dispatch(initUser);
 }, 500);
